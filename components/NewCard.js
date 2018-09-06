857bb39883c8e7103afc8ba96af9f797aa490758
Nicholas <nicholas.allio@me.com>
2018-09-06 22:02:15 +0200

    Created new components for decklist, deckcard and newdeck

---
+++ components/NewCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class NewCard extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          NewCard
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default NewCard
