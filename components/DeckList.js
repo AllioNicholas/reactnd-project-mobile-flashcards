809a336806dc0c5cc486f46bc8604041182868d6
Nicholas <nicholas.allio@me.com>
2018-09-11 23:00:05 +0200

    Connected component and displayed decks temporarly as string

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { View, Text, StyleSheet } from 'react-native'
+import { receiveDecks } from '../actions'
+import { getAllDecks } from '../utils/deckAPI'
 
 class DeckList extends Component {
+  state = {
+    ready: false
+  }
+
+  componentDidMount() {
+      const { dispatch } = this.props
+
+      getAllDecks()
+      .then((decks) => dispatch(receiveDecks(decks)))
+      .then(() => this.setState(() => ({ ready: true })))
+  }
+
   render() {
+    const decks = this.props
     return (
       <View>
         <Text>
-          DeckList
+          {JSON.stringify(decks)}
         </Text>
       </View>
     )
   }
 }
 
-export default DeckList
+function mapStateToProps(decks) {
+  return {
+    decks
+  }
+}
+
+export default connect(mapStateToProps)(DeckList)
