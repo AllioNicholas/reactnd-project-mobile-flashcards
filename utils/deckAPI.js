8e260fa485831003423b57ff107b53b625d08b42
Nicholas <nicholas@ulmon.com>
2018-09-14 15:47:18 +0200

    Temporarly cleaned deckAPIs

--- utils/deckAPI.js
+++ utils/deckAPI.js
@@ -3,14 +3,9 @@ import { AsyncStorage } from 'react-native'
 const DECKS_STORAGE_KEY = 'com.allio.nicholas.decks'
 
 export function addEntryToDeck({ card, deck }) {
-  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY,
-    JSON.stringify({
-      [deck.id]: {
-        ...deck,
-        card
-      }
-    }))
+
 }
+
 
 export function submitDeck({ key, title }) {
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY,
