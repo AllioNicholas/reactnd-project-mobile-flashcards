6474d9ada46a06bce331326c2afafefaf82a37ef
Nicholas <nicholas@ulmon.com>
2018-09-14 13:59:02 +0200

    Refined UI

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function DeckListItem ({ title, questions }) {
   return (
-    <View>
-      <Text style={{color: 'red', fontSize: 25}}>
-        {title}
-      </Text>
-      <Text>
-        {questions.length} questions
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.titleText}>
+          {title}
+        </Text>
+        <Text>
+          {questions.length} questions
+        </Text>
+      </View>
+      <View style={{ flex : 1, backgroundColor: 'silver', height: 1 }}></View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 0,
+    backgroundColor: 'white'
+  },
+  textContainer : {
+    padding: 15
+  },
+  titleText: {
+    color: 'navy',
+    fontSize: 25
+  }
+})
