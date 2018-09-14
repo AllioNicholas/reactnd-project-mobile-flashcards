c11ead23f5f2b4c0befb176bd6abb2ab765b5db5
Nicholas <nicholas@ulmon.com>
2018-09-14 16:48:14 +0200

    Added tabbar icons and navigation bar styling

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View, Platform } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons'
 import reducer from './reducers'
 
 import DeckList from './components/DeckList'
@@ -15,45 +16,79 @@ const RouteConfigs = {
   Decks: {
     screen: DeckList,
     navigationOptions: {
-      tabBarLabel: 'Decks'
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
     }
   },
   NewDeck: {
     screen: NewDeck,
     navigationOptions: {
-      tabBarLabel: 'New Deck'
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <Feather name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}
+
+const TabNavigatorConfig = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? 'navy' : 'white',
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? 'white' : 'navy',
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
     }
   }
 }
 
 const Tabs = Platform.OS === 'ios'
-? createBottomTabNavigator(RouteConfigs)
-: createMaterialTopTabNavigator(RouteConfigs)
-
-const DeckDetailNavigator = createStackNavigator(
-  {
-    DeckDetail: {
-      screen: DeckDetail
-    },
-    NewCard: {
-      screen: NewCard
-    },
-    StartQuiz: {
-      screen: Quiz
-    }
-  },
-  {
-    headerMode: 'none',
-    mode: 'modal'
-  }
-)
+? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+: createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig)
 
 const MainNavigator = createStackNavigator({
   Home: {
-    screen: Tabs
+    screen: Tabs,
+    navigationOptions: {
+      title: 'Decks',
+      headerTintColor: 'navy',
+      headerBackTitleStyle: {
+        color: 'navy',
+      }
+    }
   },
-  DeckDetailNavigator: {
-    screen: DeckDetailNavigator
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: 'navy',
+      headerBackTitleStyle: {
+        color: 'navy',
+      }
+    }
+  },
+  NewCard: {
+    screen: NewCard,
+    navigationOptions: {
+      title: 'New Card',
+      headerTintColor: 'navy',
+      headerBackTitleStyle: {
+        color: 'navy',
+      }
+    }
+  },
+  StartQuiz: {
+    screen: Quiz,
+    navigationOptions: {
+      title: 'Quiz',
+      headerTintColor: 'navy',
+      headerBackTitleStyle: {
+        color: 'navy',
+      }
+    }
   }
 })
 
