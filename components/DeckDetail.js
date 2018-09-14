44151d2890ef19307b2c68f3d492f28523830c2f
Nicholas <nicholas@ulmon.com>
2018-09-14 16:46:01 +0200

    Added style and title in navigation bar

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -4,24 +4,32 @@ import { connect } from 'react-redux'
 import { getDeck } from '../utils/deckAPI'
 
 class DeckDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params
 
+    return {
+      title: `${title}`
+    }
+  }
 
   render() {
     const { deck } = this.props
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
-          <Text>{deck.title}</Text>
-          <Text>{deck.questions.length} cards</Text>
+          <Text style={{ fontWeight: 'bold', color: 'navy', fontSize: 25 }} >{deck.title}</Text>
+          <Text style={{ color: 'silver', fontSize: 20 }}>{deck.questions.length} cards</Text>
         </View>
-        <View>
+        <View style={styles.buttonsContainer}>
           <TouchableOpacity
+            style={[styles.actionBtn, { backgroundColor: 'white', borderWidth: .5, borderColor: 'navy' }]}
             onPress={() => this.props.navigation.navigate(
                       'NewCard'
                     )} >
             <Text style={[ styles.actionBtnText, { color: 'navy'} ]}>Add Card</Text>
           </TouchableOpacity>
           <TouchableOpacity
+            style={[styles.actionBtn, { backgroundColor: 'navy' }]}
             onPress={() => this.props.navigation.navigate(
                       'StartQuiz'
                     )} >
@@ -36,32 +44,26 @@ class DeckDetail extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15
+    padding: 15,
+    justifyContent: 'space-around'
   },
-  iosActionBtn: {
+  buttonsContainer: {
+    flex: 3
+  },
+  actionBtn: {
     padding: 10,
     borderRadius: 7,
     height: 45,
     marginLeft: 40,
     marginRight: 40,
-    backgroundColor: 'navy',
-  },
-  AndroidActionBtn: {
-    padding: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    height: 45,
-    borderRadius: 2,
-    alignSelf: 'flex-end',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'navy'
+    marginBottom: 10
   },
   actionBtnText: {
     fontSize: 22,
     textAlign: 'center',
   },
   titleContainer: {
+    flex: 3,
     alignItems: 'center',
     justifyContent: 'center'
   }
