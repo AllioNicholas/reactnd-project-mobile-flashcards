93b89282bc70729b9e553dc5076a19d294382c76
Nicholas <nicholas@ulmon.com>
2018-09-17 14:29:48 +0200

    Styled action button to look move similar to host OS

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
 
 export default function ActionButton ({ onPress, title, buttonStyle, textStyle, ...props }) {
   return (
     <TouchableOpacity
       {...props}
       onPress={onPress}
-      style={[styles.submitBtn, buttonStyle]}>
-      <Text style={[styles.submitBtnText, textStyle]}>{title}</Text>
+      style={[Platform.OS === 'ios' ? styles.iosBtn : styles.AndroidBtn, buttonStyle]}>
+      <Text style={[styles.btnText, textStyle]}>{title}</Text>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-  submitBtn: {
+  iosBtn: {
     padding: 10,
     borderRadius: 7,
     height: 45,
@@ -21,9 +21,19 @@ const styles = StyleSheet.create({
     marginRight: 40,
     backgroundColor: 'navy',
   },
-  submitBtnText: {
+  btnText: {
     fontSize: 22,
     textAlign: 'center',
     color: 'white'
+  },
+  AndroidBtn: {
+    backgroundColor: 'white',
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
